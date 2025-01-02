ee906149b295603e62907e2bae2986a1adcefc66
SreeLakshmi1242 <166686220+SreeLakshmi1242@users.noreply.github.com>
2025-01-03 00:15:07 +0530

    Update PatientDashboard.py

--- PatientDashboard/PatientDashboard.py
+++ PatientDashboard/PatientDashboard.py
@@ -167,7 +167,7 @@ with col_c1:
     })
 
     # Output the concatenated result
-    st.write("Combined Diagnosis and Prescriptions:", diag_pres)
+    # st.write("Combined Diagnosis and Prescriptions:", diag_pres)
 
     diagnosis = 'Diagnosis : ' +','.join(diag_pres["Diagnosis Codes"].unique())
     prescription = 'Prescriptions : ' + ','.join(diag_pres["Prescriptions"].unique())
