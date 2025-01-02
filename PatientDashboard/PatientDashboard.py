48f42027f1693494a618f2f599a94c40efa05521
SreeLakshmi1242 <166686220+SreeLakshmi1242@users.noreply.github.com>
2025-01-03 00:24:05 +0530

    Update PatientDashboard.py

--- PatientDashboard/PatientDashboard.py
+++ PatientDashboard/PatientDashboard.py
@@ -169,8 +169,8 @@ with col_c1:
     # Output the concatenated result
     # st.write("Combined Diagnosis and Prescriptions:", diag_pres)
 
-    diag_pres["Diagnosis Codes"] = diag_pres["Diagnosis Codes"].str.replace(" ", "")
-    diag_pres["Prescriptions"] = diag_pres["Prescriptions"].str.replace(" ", "")
+    diag_pres["Diagnosis Codes"] = diag_pres["Diagnosis Codes"].str.replace(" ", "").lower()
+    diag_pres["Prescriptions"] = diag_pres["Prescriptions"].str.replace(" ", "").lower()
     
     diagnosis = 'Diagnosis : '+', '.join(diag_pres["Diagnosis Codes"].unique())
     prescription = 'Prescriptions : '+', '.join(diag_pres["Prescriptions"].unique())
